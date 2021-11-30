778907c88c9cb8f9f9458e3f5e840117b3ee83dd
Ignacio J. Ortega <nacho.ortega@gmail.com>
2021-11-30 17:17:44 +0100

    [UPD] removed -d param for docker added $2 to make docker exit inmmediately

--- bbb-mp4.sh
+++ bbb-mp4.sh
@@ -12,8 +12,8 @@ echo "converting $MEETING_ID to mp4" |  systemd-cat -p warning -t bbb-mp4
 docker rm -f $MEETING_ID
 rm -f $COPY_TO_LOCATION/$MEETING_ID.mp4
 
-sudo docker run --rm -d \
-                --name $MEETING_ID \
+sudo docker run --rm $2 \
+                --name bbb-mp4-$MEETING_ID \
                 -v $COPY_TO_LOCATION:/usr/src/app/download \
                 --env-file  $(pwd)/.env \
                 --env REC_URL=https://$BBB_DOMAIN_NAME/playback/presentation/2.3/$MEETING_ID?meetingID=$MEETING_ID \
